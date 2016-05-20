cf18bd9702cb84ce8cd1196344798d3d90462510
wat-aro <kazutas1008@gmail.com>
2016-05-20 14:54:33 +0900

    Fix localization view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: 'Account activation'
+    mail to: user.email, subject: t('account_activation')
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: 'Password reset'
+    mail to: user.email, subject: t('password_reset.title')
   end
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -5,7 +5,7 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:michael)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal 'Account activation', mail.subject
+    assert_equal I18n.t('account_activation'), mail.subject
     assert_equal [user.email], mail.to
     assert_equal ['noreply@example.com'], mail.from
     assert_match user.name, mail.body.encoded
@@ -17,7 +17,7 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:michael)
     user.reset_token = User.new_token
     mail = UserMailer.password_reset(user)
-    assert_equal 'Password reset', mail.subject
+    assert_equal I18n.t('password_reset.title'), mail.subject
     assert_equal [user.email], mail.to
     assert_equal ['noreply@example.com'], mail.from
     assert_match user.reset_token, mail.body.encoded
